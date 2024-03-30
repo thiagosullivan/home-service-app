9b063f0da523a731610187f8fc2d1b5a91ea2a10
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-03-30 08:37:33 -0300

    feat: add auth and token for persisting logged in

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './App/Screens/LoginScreen/Login';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-expo';
+import Constants from "expo-constants"
+import * as SecureStore from "expo-secure-store";
+
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey="pk_test_ZGlzY3JldGUtbWFjYXctMzEuY2xlcmsuYWNjb3VudHMuZGV2JA"
+    >
+      <View style={styles.container}>
+        
+        {/* Sign in Component */}
+        <SignedIn>
+          <Text>You are Signed in</Text>
+        </SignedIn>
+        {/* Sign Out Component */}
+        <SignedOut>
+          <Login />
+        </SignedOut>
+
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
@@ -14,7 +50,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
